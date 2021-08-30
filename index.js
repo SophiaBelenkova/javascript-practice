a012fa49b4615c989fde93f8fcde28e713db5e45
Sophia Belenkova <sophiabelenkova@MacBook-Pro-Sophia.local>
2021-08-30 17:10:22 +0300

    separate js file used

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const person = {
-    name: 'Elon Musk',
-    age: 50,
-    levitate() {
-        alert(`${person.name} is levitating`);
-    }
-}
+
+    let counter = 0;
+
+    document.addEventListener('DOMContentLoaded', function(){
+        document.querySelector('button').onclick = levitate;
+    });
+       
+
+    function levitate() {
+        if (document.querySelector('h1').innerHTML === 'Elon Musk is levitating!') {
+            document.querySelector('h1').innerHTML = 'Elon Musk has landed on Mars!'
+        } else {
+            document.querySelector('h1').innerHTML = 'Elon Musk is levitating!'
+        }
+        counter++;
+        document.querySelector('div').innerHTML = counter;
+        document.querySelector('h2').innerHTML + counter;
+    };
