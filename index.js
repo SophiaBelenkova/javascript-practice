3171dbb5628ae2d995319c70d2e367b11ac49f31
Sophia Belenkova <sophiabelenkova@MacBook-Pro-Sophia.local>
2021-08-30 19:00:06 +0300

    submit form added

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 
-    let counter = 0;
+let counter = 0;
 
-    document.addEventListener('DOMContentLoaded', function(){
-        document.querySelector('button').onclick = levitate;
-    });
-       
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').onclick = levitate;
 
-    function levitate() {
-        if (document.querySelector('h1').innerHTML === 'Elon Musk is levitating!') {
-            document.querySelector('h1').innerHTML = 'Elon Musk has landed on Mars!'
-        } else {
-            document.querySelector('h1').innerHTML = 'Elon Musk is levitating!'
-        }
-        counter++;
-        document.querySelector('div').innerHTML = counter;
-        document.querySelector('h2').innerHTML + counter;
+    document.querySelector('form').onsubmit = function () {
+        const name = document.querySelector('#name').value;
+        alert(`Wait, ${name}, Elon\'s gonna pick you up on Jupiter`);
     };
+});
+
+
+function levitate() {
+    if (document.querySelector('h1').innerHTML === 'Elon Musk is levitating!') {
+        document.querySelector('h1').innerHTML = 'Elon Musk has landed on Mars!'
+    } else {
+        document.querySelector('h1').innerHTML = 'Elon Musk is levitating!'
+    }
+    counter++;
+    document.querySelector('div').innerHTML = counter;
+    document.querySelector('h2').innerHTML + counter;
+};
+
