7bf2923fb4844dacf5cc946ee9f5a33f41637815
Sophia Belenkova <sophiabelenkova@MacBook-Pro-Sophia.local>
2021-08-31 14:04:44 +0300

    submit form changes

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 
 let counter = 0;
-
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('button').onclick = levitate;
 
-    document.querySelector('form').onsubmit = function () {
+    document.querySelector('#namesubmit').onsubmit = function () {
         const name = document.querySelector('#name').value;
-        alert(`Wait, ${name}, Elon\'s gonna pick you up on Jupiter`);
+        if(name.length > 0){
+            alert(`Wait, ${name}, Elon\'s gonna pick you up on Jupiter`);
+        }else if(name.length === 0){
+            alert('Please, enter your name');
+        }
+        
     };
+
+    document.querySelector('#colorchanger').onclick = function(){
+        document.querySelector('h1').style.color = 'blue';
+    }
 });
 
 
 function levitate() {
-    if (document.querySelector('h1').innerHTML === 'Elon Musk is levitating!') {
-        document.querySelector('h1').innerHTML = 'Elon Musk has landed on Mars!'
-    } else {
-        document.querySelector('h1').innerHTML = 'Elon Musk is levitating!'
-    }
+    document.querySelector('h1').innerHTML = 'Elon Musk is levitating!';
     counter++;
     document.querySelector('div').innerHTML = counter;
     document.querySelector('h2').innerHTML + counter;
 };
-
